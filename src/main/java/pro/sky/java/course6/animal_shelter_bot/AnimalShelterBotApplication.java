63bc4c512f17bc9e45bdf9d0a12168bbc37599d2
shulga_ea <shulga23@gmail.com>
2022-11-15 17:33:29 +0300

    feature-command1

--- src/main/java/pro/sky/java/course6/animal_shelter_bot/AnimalShelterBotApplication.java
+++ src/main/java/pro/sky/java/course6/animal_shelter_bot/AnimalShelterBotApplication.java
@@ -2,8 +2,10 @@ package pro.sky.java.course6.animal_shelter_bot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class AnimalShelterBotApplication {
 
     public static void main(String[] args) {
